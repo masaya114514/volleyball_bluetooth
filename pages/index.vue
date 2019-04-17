a679334ded9183bc75154a8df75f539cb676184e
masay <masaya0322713181@ezweb.ne.jp>
2019-04-18 00:39:16 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+    <v-btn color="warning" dark>Normal Button</v-btn>
+    
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo,
+  }
+}
+</script>
